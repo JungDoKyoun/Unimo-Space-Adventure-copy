208d84d04c41910a4ddfef1f993e4bea7f567f1a
jamjamstone <junghyun4545@naver.com>
2025-05-09 14:33:27 +0900

    [Add] 기초적인 이동기능 추가
    키보드 wasd나 vr컨트롤러의 왼쪽 조이스틱을 이용한 기초적인 회전기능 추가 부드러운 회전기능이나 장애물 정지기능은 추가 필요

--- Assets/WorkSpace/YJH/Scripts/PlayerActionManager.cs
+++ Assets/WorkSpace/YJH/Scripts/PlayerActionManager.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class PlayerActionManager : MonoBehaviour
 {
+    public float itemDetectionRange = 5f;//temp value
+    private LayerMask itemLayerMask;
+    private GameObject targetObject;
     // Start is called before the first frame update
     void Start()
     {
@@ -31,7 +34,53 @@ public class PlayerActionManager : MonoBehaviour
     {
         StartCoroutine(GatheringCoroutine());
     }
+    IEnumerator FindItem()
+    {
+        while (true)
+        {
+            
+            yield return new WaitForSeconds(0.01f);
+            Collider[] detectedColliders = Physics.OverlapSphere(transform.position, itemDetectionRange, itemLayerMask);
+            if(detectedColliders.Length > 0)
+            {
+                float distance=float.MaxValue;
+                foreach(Collider collider in detectedColliders)
+                {
+                    float distanceBetween= Vector3.Distance(transform.position, collider.transform.position);
+                    if (distance> distanceBetween)
+                    {
+                        distance = distanceBetween;
+                        targetObject=collider.gameObject;
+                    }
+                    else if(distance==distanceBetween)
+                    {
+                        if (targetObject!=null)
+                        {
+                            if (true)
+                            {
 
+                            }
+                            else
+                            {
+
+                            }
+                        }
+                        
+
+                        
+                    }
+                }
+
+
+
+            }
+            else
+            {
+
+            }
+
+        }
+    }
     IEnumerator GatheringCoroutine()// 아이템 채집중 사용할 코로틴
     {
         yield return null;
--- Assets/WorkSpace/YJH/Scripts/PlayerCharacterMoveManager.cs
+++ Assets/WorkSpace/YJH/Scripts/PlayerCharacterMoveManager.cs
@@ -1,14 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.XR;
+using UnityEngine.Windows;
+using Unity.XR;
+using Unity.VisualScripting;
+
 
 public class PlayerCharacterMoveManager : MonoBehaviour
 {
-    Rigidbody playerCharacterBody;
-    Vector3 playerMoveDirection;
-    Transform playerTransform;
-    
+    private Rigidbody playerCharacterBody;
+    private Vector3 playerMoveDirection;
+    private Vector3 playerPushDirection;
+    private Transform playerTransform;
+    [SerializeField] AudioSource moveSoundClip;
+    [SerializeField] bool isMoveSoundPlay=false;
 
+    [Header("¼Óµµµé")]
+    [SerializeField] float moveSpeed = 4f;
+    [SerializeField] float pushSpeed = 3f;
+    [SerializeField] float rotateSpeed = 10000f;
 
 
 
@@ -22,20 +34,91 @@ public class PlayerCharacterMoveManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        PlayerMoveBySpeed();
     }
 
     public void PlayerMoveBySpeed()
     {
+        if (playerMoveDirection.magnitude>float.Epsilon)
+        {
+            isMoveSoundPlay = true;
+        }
+        else
+        {
+            isMoveSoundPlay = false;
+        }
+        transform.position += moveSpeed * Time.deltaTime * playerMoveDirection + pushSpeed * Time.deltaTime * playerPushDirection;
+        
 
+
+
+        if (isMoveSoundPlay)
+        {
+            moveSoundClip?.Play();
+        }
+        else
+        {
+            moveSoundClip?.Stop();
+        }
+        //playerTransform.position += moveSpeed * Time.deltaTime * moveDir + pushSpeed * Time.deltaTime * pushDir;
+        //moveSoundClip.volume = moveSoundMax * Mathf.Clamp01(moveDir.magnitude);
+        //moveSoundClip.volume = Sound_Manager.instance._audioSources[1].volume;
+        //
+        //if (mapSetter.IsInMap(playerTransform.position) == false)
+        //{
+        //    playerTransform.position = mapSetter.FindNearestPoint(playerTransform.position);
+        //}
+        //auraCtrl.transform.position = playerTransform.position + new Vector3(0f, auraOffset, 0f);
+        //if (pushDir.magnitude < 0.01f)
+        //{
+        //    changeRotation(moveDir);
+        //}
     }
-    public void PlayerMoveByPush()
+    public void SetMoveSoundPlayOn()
     {
+        isMoveSoundPlay = true; 
+    }
+    public void SetMoveSoundPlayOff()
+    {
+        isMoveSoundPlay = false;
+    }
 
+
+    public void PlayerMoveByPush(Vector3 pushVector)
+    {
+        if (pushVector.magnitude <float.Epsilon)
+        {
+            playerPushDirection = Vector3.zero;
+            return;
+        }
+
+        playerPushDirection = pushVector;   
     }
     public void SetTransform(Transform transform)
     {
         playerTransform = transform;    
+    }
+
+    public void GetRotate(Vector2 headDirection)
+    {
+        //Debug.Log("rot");
+        Vector3 headingVector3 = new Vector3(headDirection.x, 0, headDirection.y);
+        //Debug.Log(headingVector3);
+        Quaternion nextRotation=Quaternion.LookRotation(headingVector3);
+        Quaternion firstRotation = Quaternion.LookRotation(new Vector3(transform.forward.x,0,transform.forward.z));
+        //Debug.Log(nextRotation);
+        //transform.rotation = Quaternion.Slerp(firstRotation, nextRotation, Time.deltaTime*rotateSpeed);
+        transform.rotation=nextRotation;
+    }
+
+
+    
+    public void OnMove(InputValue value)
+    {
+        
+        Vector2 inputVector = value.Get<Vector2>();
+        GetRotate(inputVector);
+        playerMoveDirection = new Vector3(inputVector.x,0,inputVector.y);
     }
     
 }
