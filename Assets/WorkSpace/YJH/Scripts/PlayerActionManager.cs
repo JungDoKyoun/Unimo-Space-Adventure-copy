62481785b36d91416ca40dceda3968573a6aa069
jamjamstone <junghyun4545@naver.com>
2025-05-12 13:34:21 +0900

    [Add] 기초적인 채취 기능 작성
    채취하면서 체력을 감소시키는 로직 추가

--- Assets/WorkSpace/YJH/Scripts/PlayerActionManager.cs
+++ Assets/WorkSpace/YJH/Scripts/PlayerActionManager.cs
@@ -22,20 +22,29 @@ public partial class PlayerManager : MonoBehaviour
 
     private GameObject targetObject;
     // Start is called before the first frame update
+
+    private bool isGatheringCoroutineWork = false;
+
+    public delegate void OnTargetSet();
+    public event OnTargetSet OnTargetObjectSet;
+
     
     public void ActionStart()
     {
         gatheringAudioSource.clip = gatheringAudioClip;
         StartDetectItem();
+        OnTargetObjectSet += GatheringItem;
     }
     // Update is called once per frame
     
     public void ActionUpdate()
     {
-
+        
+        
     }
     public void StartDetectItem()
     {
+        
         StartCoroutine(FindItem());
     }
 
@@ -48,7 +57,16 @@ public partial class PlayerManager : MonoBehaviour
 
     public void GatheringItem()
     {
-        StartCoroutine(GatheringCoroutine());
+        Debug.Log("gathering2");
+        if (isGatheringCoroutineWork == false)
+        {
+            isGatheringCoroutineWork = true;
+            StartCoroutine(GatheringCoroutine());
+        }
+        else
+        {
+            return;
+        }
     }
     IEnumerator FindItem()
     {
@@ -57,12 +75,12 @@ public partial class PlayerManager : MonoBehaviour
             yield return new WaitForSeconds(0.1f);
             if (targetObject == null)
             {
-                Debug.Log("null");
+                //Debug.Log("null");
                 isGathering = false;
             }
             else
             {
-                Debug.Log(Vector3.Distance(transform.position, targetObject.transform.position));
+               // Debug.Log(Vector3.Distance(transform.position, targetObject.transform.position));
                 if (Vector3.Distance(transform.position, targetObject.transform.position) > itemDetectionRange)
                 {
 
@@ -73,7 +91,7 @@ public partial class PlayerManager : MonoBehaviour
 
             while (isGathering == false)
             {
-                Debug.Log("enterwhile");
+                //Debug.Log("enterwhile");
                 yield return new WaitForSeconds(0.1f);
                 Collider[] detectedColliders = Physics.OverlapSphere(transform.position, itemDetectionRange, itemLayerMask);
 
@@ -81,8 +99,8 @@ public partial class PlayerManager : MonoBehaviour
 
                 if (detectedColliders.Length > 0)
                 {
-                    Debug.Log("detected more than 1");
-                    Debug.Log(detectedColliders[0].gameObject.name);
+                    //Debug.Log("detected more than 1");
+                    //Debug.Log(detectedColliders[0].gameObject.name);
                     float distance = float.MaxValue;
                     foreach (Collider collider in detectedColliders)
                     {
@@ -127,7 +145,7 @@ public partial class PlayerManager : MonoBehaviour
 
 
                     isGathering = true;
-
+                    OnTargetObjectSet?.Invoke();
                 }
                 else
                 {
@@ -140,20 +158,30 @@ public partial class PlayerManager : MonoBehaviour
     }
     IEnumerator GatheringCoroutine()// 아이템 채집중 사용할 코로틴
     {
+        IGatheringObject targetScript = null;
         if (targetObject != null)
         {
-            var targetScript= targetObject.GetComponent<IGatheringObject>();
+             targetScript= targetObject.GetComponent<IGatheringObject>();
         }
         while (true)
         {
+            Debug.Log("gathering");
             yield return new WaitForSeconds(gatheringDelay);
-            
-            //타겟 오브젝트의 스크립트를 가져와서 타겟 스크립트에 저장
-            //타겟 스크립트의 체력을 gatheringSpeed에 비례하게 감소 -> 정확하게 gatheringSpeed만큼 감소할 필요는 없기 때문
-            //만약 타겟 스크립트의 체력이 0보다 아래로 내려가면
-            //타겟 오브젝트를 null로 지정하고
-            //재화를 증가시키고
-            //이동 매니저에 채취중을 활성화 시킨다
+            targetScript.NowHP -= gatheringSpeed;
+            if(targetScript.NowHP < 0)
+            {
+                targetScript.OnGatheringEnd();
+                targetObject = null;
+                isGathering= false;
+                isGatheringCoroutineWork = false;
+                yield break;
+            }
+            //타겟 오브젝트의 스크립트를 가져와서 타겟 스크립트에 저장 s
+            //타겟 스크립트의 체력을 gatheringSpeed에 비례하게 감소 -> 정확하게 gatheringSpeed만큼 감소할 필요는 없기 때문s
+            //만약 타겟 스크립트의 체력이 0보다 아래로 내려가면s
+            //타겟 오브젝트를 null로 지정하고s
+            //재화를 증가시키고 ->use에 
+            //이동 매니저에 채취중을 활성화 시킨다s
 
 
 
