b77087404d767d6e9802695928c400322484f316
JungDoKyoun <kyous112@naver.com>
2025-05-21 12:27:49 +0900

    [feat] 복구

--- Assets/WorkSpace/JDG/Script/TileData.cs
+++ Assets/WorkSpace/JDG/Script/TileData.cs
@@ -33,8 +33,42 @@ namespace JDG
         public EnvironmentType EnvironmentType { get { return _environmentType; } set { _environmentType = value; } }
         public bool IsCleared { get { return _isCleared; } set { _isCleared = value; } }
         public string SceneName { get { return _sceneName; } set { _sceneName = value; } }
+<<<<<<< HEAD
         public ModeType ModeType { get { return _modeType; } set { _modeType = value; } }
         public EventType EventType { get { return _eventType; } set { _eventType = value; } }
         public int Level { get { return _level; } set { _level = value; } }
+=======
+        public string ModeName { get { return _modeName; } set { _modeName = value; } }
+
+        public Dictionary<string, object> ToDictionary()
+        {
+            return new Dictionary<string, object>
+            {
+                { "CoordX", Coord.x },
+                { "CoordY", Coord.y },
+                { "TileType", (int)TileType }, // enum은 int로 변환
+                { "TileVisibility", (int)TileVisibility },
+                { "EnvironmentType", (int)EnvironmentType },
+                { "IsCleared", IsCleared },
+                { "Level", Level },
+                { "SceneName", SceneName },
+                { "ModeName", ModeName }
+            };
+        }
+
+        public static TileData FromDictionary(Dictionary<string, object> dict)
+        {
+            Vector2Int coord = new Vector2Int(Convert.ToInt32(dict["CoordX"]), Convert.ToInt32(dict["CoordY"]));
+            TileType tileType = (TileType)Convert.ToInt32(dict["TileType"]);
+            TileVisibility visibility = (TileVisibility)Convert.ToInt32(dict["TileVisibility"]);
+            EnvironmentType envType = (EnvironmentType)Convert.ToInt32(dict["EnvironmentType"]);
+            bool isCleared = Convert.ToBoolean(dict["IsCleared"]);
+            int level = Convert.ToInt32(dict["Level"]);
+            string sceneName = dict["SceneName"]?.ToString();
+            string modeName = dict["ModeName"]?.ToString();
+
+            return new TileData(coord, tileType, visibility, envType, isCleared, level, sceneName, modeName);
+        }
+>>>>>>> parent of bee8db1 (Merge branch 'lee_ze' into Develop)
     }
 }
