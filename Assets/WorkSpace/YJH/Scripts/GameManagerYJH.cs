a3e01b8954a72bd0df741447555de65a2540f41a
jamjamstone <junghyun4545@naver.com>
2025-05-22 17:39:29 +0900

    [Chore] for merge2

--- Assets/WorkSpace/YJH/Scripts/GameManagerYJH.cs
+++ Assets/WorkSpace/YJH/Scripts/GameManagerYJH.cs
@@ -13,6 +13,9 @@ public partial class GameManager : MonoBehaviour
     public PlayerStatus playerStatus= new PlayerStatus();
     [SerializeField] PlayerStatus originPlayerStatus= new PlayerStatus();
     public PlayerStatus OriginPlayerStatus { get {  return originPlayerStatus; } }
+    private List<BuildEffect> buildEffects = new List<BuildEffect>();
+
+    
     private void Awake()
     {
         if(Instance == null)
@@ -32,9 +35,63 @@ public partial class GameManager : MonoBehaviour
         playerStatus= originPlayerStatus;
     }
 
-    // Update is called once per frame
-    void Update()
+   
+   
+    public void AddStatEffect(BuildEffect buildEffect)
     {
+        buildEffects.Add(buildEffect);
+    }
+    public void ModifieStat()
+    {
+        float speedSum=originPlayerStatus.moveSpeed;
+        float maxHPSum = originPlayerStatus.maxHP;
+        float gatherSpeedSum=originPlayerStatus.gatheringSpeed;
+        float gatherDelaySum=originPlayerStatus.gatheringDelay;
+        float damageSum=originPlayerStatus.playerDamage;
+        float gatherRangeSum=originPlayerStatus.itemDetectionRange;
         
+        foreach (var buildeffect in buildEffects)
+        {
+            switch(buildeffect)
+            {
+                case Speed speed:
+                    speedSum += buildeffect.ReturnFinalStat(originPlayerStatus.moveSpeed);
+
+                    break;
+                case MaxHp maxHP:
+                    maxHPSum += buildeffect.ReturnFinalStat(originPlayerStatus.maxHP);
+
+                    break;
+                case GatheringSpeed gatheringSpeed:
+                    gatherSpeedSum += buildeffect.ReturnFinalStat(originPlayerStatus.gatheringSpeed);
+                    break;
+                case GatheringDelay gatheringDelay:
+                    gatherDelaySum += buildeffect.ReturnFinalStat(originPlayerStatus.gatheringDelay);
+                    break;
+                case Damage damage:
+                    damageSum += buildeffect.ReturnFinalStat(originPlayerStatus.playerDamage);
+                    break;
+                case ItemDetectionRange itemDetectionRange:
+                    gatherRangeSum += buildeffect.ReturnFinalStat(originPlayerStatus.itemDetectionRange);
+                    break;
+                default:
+
+                    break;
+
+            }
+        }
+        
+        playerStatus=originPlayerStatus.Clone();
+        playerStatus.moveSpeed += speedSum;
+        playerStatus.maxHP += maxHPSum;
+        playerStatus.gatheringSpeed += gatherSpeedSum;
+        playerStatus.gatheringDelay += gatherDelaySum;
+        playerStatus.playerDamage += damageSum;
+        playerStatus.itemDetectionRange += gatherRangeSum;
+
+
+
+
+
     }
 }
