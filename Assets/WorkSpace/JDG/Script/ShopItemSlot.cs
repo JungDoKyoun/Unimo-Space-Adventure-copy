971627b17f86a53cdda9d267653e61923db5c3a1
JungDoKyoun <kyous112@naver.com>
2025-05-26 15:37:56 +0900

    [fix] 실수로 저장 안한 상점관련 스크립트

--- Assets/WorkSpace/JDG/Script/ShopItemSlot.cs
+++ Assets/WorkSpace/JDG/Script/ShopItemSlot.cs
@@ -34,9 +34,6 @@ namespace JDG
 
         public void OnBuyButtonClicked()
         {
-            if (_isBuy)
-                return;
-
             ResourcesType resourcesType = _relicData._relicPrice._resourceType; //아이템 자원 타입
             int relicPrice = _relicData._relicPrice._value; //아이템 가격
             Debug.Log("구매 완료");
@@ -44,7 +41,6 @@ namespace JDG
             //플레이어 소지 자원에서 아이템가격 깍기 및 효과추가
 
             //구매에 성공했을때 구매버튼 비활성화 위에서 구매 성공시 아래 코드 마지막 구매성공 코드에 넣고 아래 코드 지우기
-            _isBuy = true;
             _disabledOverlay.SetActive(true);
             _buyButton.interactable = false;
         }
