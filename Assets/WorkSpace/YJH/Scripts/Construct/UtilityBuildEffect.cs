1ccaa9c9c62a4fdac7ea08b2b865dbbcb3fa94f5
jamjamstone <junghyun4545@naver.com>
2025-06-05 14:32:08 +0900

    [Add] CSV 파일을 통한 건축 시스템 변경 로직 추가
    csv 파일을 통해서 건물의 설명등을 교체하는 시스템 추가

---
+++ Assets/WorkSpace/YJH/Scripts/Construct/UtilityBuildEffect.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UtilityBuildEffect : ScriptableObject, IUtilityBuildEffect
+{
+    public Action IUtilityBuildEffect()
+    {
+        return null;
+    }
+}
