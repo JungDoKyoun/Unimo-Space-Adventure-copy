5e9cecd361da59fd74221eaf3b54075feb279292
2jaeyoung2 <80027417+2jaeyoung2@users.noreply.github.com>
2025-06-11 13:58:28 +0900

    [Temp]

--- Assets/WorkSpace/lee_ze/01. Scripts/Firebase Manager/FirebaseDataBaseMgr.cs
+++ Assets/WorkSpace/lee_ze/01. Scripts/Firebase Manager/FirebaseDataBaseMgr.cs
@@ -444,6 +444,8 @@ public class FirebaseDataBaseMgr : MonoBehaviour
 
     public IEnumerator UpdateMyScore(float currentScore)
     {
+        Score = currentScore;
+
         var getTask = dbRef.Child("users").Child(user.UserId).Child("score").GetValueAsync();
 
         yield return new WaitUntil(predicate: () => getTask.IsCompleted);
@@ -457,14 +459,20 @@ public class FirebaseDataBaseMgr : MonoBehaviour
 
         if (getTask.Result.Exists == true && float.TryParse(getTask.Result.Value.ToString(), out float savedScore)) // string으로 불러온 ingame currency를 tryparse로 savedValue에 저장
         {
-            if (currentScore > savedScore) // 기존 기록보다 tempScore(방금 세운 기록)이 크면
+            if (Score > savedScore) // 기존 기록보다 currentScore(방금 세운 기록)이 크면
             {
-                var DBTask = dbRef.Child("user").Child(user.UserId).Child("score").SetValueAsync(currentScore);
+                // score 최신화
+                var DBTask = dbRef.Child("user").Child(user.UserId).Child("score").SetValueAsync(Score);
+
+                yield return new WaitUntil(predicate: () => DBTask.IsCompleted);
+
+                // 전체 랭킹 업데이트
+                StartCoroutine(UpdateRankingList());
             }
         }
     }
 
-    private void UpdateRankingList() // station 씬 진입 시 호출 될 함수
+    private IEnumerator UpdateRankingList() // station 씬 진입 시 호출 될 함수
     {
 
     }
