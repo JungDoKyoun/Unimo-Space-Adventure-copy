b1c410caceba0b2676ab2ede2f4e4d0e5bc7eb10
jamjamstone <junghyun4545@naver.com>
2025-06-25 14:10:29 +0900

    [Add] 건설여부 시각화 로직 추가
    건설 여부에 따라서 버튼의 색상을 변화시키는 로직 추가

--- Assets/WorkSpace/YJH/Scripts/Construct/ConstructUIManager.cs
+++ Assets/WorkSpace/YJH/Scripts/Construct/ConstructUIManager.cs
@@ -58,6 +58,7 @@ public class ConstructUIManager : MonoBehaviour
             Instantiate(constructManager);
         }
         SetAllButtons();
+        ChangeButtonsSpriteAll();
     }
 
     public void SetAllButtons()
